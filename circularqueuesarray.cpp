f8f159d44bbaecfc56472b1b3d1d28df02d478bb
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-29 14:23:21 +0700

    memasukkan data ke queue

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -35,7 +35,7 @@ public:
             FRONT = 0;
             REAR = 0; 
          }
-         
+
         else
         {
             // jika rear berada di posisi terakhir array, kembali ke awal array
@@ -43,3 +43,5 @@ public:
                 REAR = 0;
             else
                 REAR = REAR + 1;
+             }
+        queue_array[REAR] = num;    
