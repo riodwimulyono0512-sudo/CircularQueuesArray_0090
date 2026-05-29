462fe1a87c87df62df88e25ff84b7180e7390d95
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-29 14:31:48 +0700

    jika FRONT > REAR, iterasi dari FRONT hingga akhir array

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -97,3 +97,13 @@ public:
             }
             cout << endl;
         }
+
+        else
+        {
+            // jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            FRONT_Position = 0;
