13e49b4de7a4e97f6768c2b0b5779b1d2340a387
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-29 14:34:33 +0700

    finish

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -123,4 +123,56 @@ int main()
 {
     Queues q;
     char ch;
-    
+
+     while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement Insert operasi" << endl;
+            cout << "2. Implement Delete Operasi" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice(1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+
+            case '3':
+            {
+                q.display();
+                break;
+            }
+
+            case '4':
+            {
+                return 0;
+            }
+            
+            default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &ex)
+        {
+            cout << "Check for the values entered!!" << endl;
+        }
+    }
+    return 0;
+}
