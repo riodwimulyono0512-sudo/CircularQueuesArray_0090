a57f1df23a459e77b9ffe3d20d3b4958a735fad0
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-29 14:22:42 +0700

    jika rear berada diposisi terakhir array, kembali ke awal array

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -28,9 +28,18 @@ public:
             cout << "Queue overflow";
             return; 
             }
-            
+
         // cek apakah antrian kosong
         if (FRONT == -1)
         {
             FRONT = 0;
             REAR = 0; 
+         }
+         
+        else
+        {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
