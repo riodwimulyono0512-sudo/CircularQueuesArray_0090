693827ab735ba1f7941c73a465c4b7feccb7455f
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-29 14:17:26 +0700

    public

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -6,3 +6,10 @@ class Queues //membuat class queues
 private:
 static const int max = 3;
   int FRONT, REAR;
+  int queue_array[max];
+  
+public:
+    Queues()     
+    {
+        FRONT = -1;
+        REAR = -1;
