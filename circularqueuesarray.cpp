3d46e4c42641a83da03c2fed8c0cd72b1c7e1584
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-29 14:32:22 +0700

    literasi dari awal array hingga rear

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -107,3 +107,14 @@ public:
                 FRONT_Position++;
             }
             FRONT_Position = 0;
+           
+             // iterasi dari awal array hingga REAR
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
+    }
+};
